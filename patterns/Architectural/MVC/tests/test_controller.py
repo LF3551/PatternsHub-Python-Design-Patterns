5eb1b15b0b262cb63db7342cb05d2c0829603e8c
Alexey Aleynikov <alexey.aleynikov@telekom.com>
2024-05-12 11:22:59 +0300

    added mvc code and test

--- patterns/Architectural/MVC/tests/test_controller.py
+++ patterns/Architectural/MVC/tests/test_controller.py
@@ -18,8 +18,8 @@ class TestController(unittest.TestCase):
 
     @patch('MVC.Controller.controller.display_error')
     def test_add_existing_item(self, mock_display):
-        self.controller.add_item('test', 'value')  # Первый вызов, создание элемента
-        self.controller.add_item('test', 'value')  # Второй вызов, элемент уже существует
+        self.controller.add_item('test', 'value')
+        self.controller.add_item('test', 'value')
         mock_display.assert_called_with('Item already exists')
 
 if __name__ == '__main__':
